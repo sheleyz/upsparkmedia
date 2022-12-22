bda9f078063afe9a3c66447bad1d78c21739188b
sheleyz <59847602+sheleyz@users.noreply.github.com>
2022-12-21 19:01:31 -0500

    Switch from Netlify CMS to Contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,23 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require("dotenv").config();
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`
+});
 module.exports = {
-  siteMetadata: {
-    title: `UpSpark Media`,
-    siteUrl: `https://upsparkmedia.com/`
-  },
-  plugins: ["gatsby-plugin-netlify-cms"]
+    siteMetadata: {
+        title: `UpSpark Media`,
+        siteUrl: `https://upsparkmedia.com/`
+    },
+    plugins: [
+        "gatsby-plugin-image",
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+            }
+        }
+    ]
 };
