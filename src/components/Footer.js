bc9c8dabe4994cf258870e11845568a5082a57d2
sheleyz <59847602+sheleyz@users.noreply.github.com>
2022-12-26 16:38:15 -0500

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
+import { faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
+
+function Footer() {
+    return (
+        <footer className="bg-brand-secondary py-5 text-white">
+            <div className="container px-4 px-lg-5 fw-semibold">
+                <div className="text-center">UpSpark Media</div>
+                <div className="text-center">
+                    <a className="btn btn-social mx-2 my-3" href="https://twitter.com/upsparkmedia" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faTwitterSquare} size="3x" fixedWidth />
+                    </a>{" "}
+                    <a className="btn btn-social mx-2 my-3" href="https://instagram.com/upspark.media" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faInstagramSquare} size="3x" fixedWidth />
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import * as React from "react";
 import "../index.scss";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
     return (
         <div>
             <Header />
             <main>{children}</main>
+            <Footer />
         </div>
     );
 };
