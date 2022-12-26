0f75ab4ee94928c2d3063a39823a3c4fe8263570
sheleyz <59847602+sheleyz@users.noreply.github.com>
2022-12-26 16:11:47 -0500

    Added contact section

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ const IndexPage = () => {
             {/* Services Section */}
             <section className="page-section bg-brand-light" id="services">
                 <div className="container px-4 px-lg-5">
-                    <h2 className="text-center mt-0 text-brand-secondary">Services</h2>
+                    <h2 className="text-center mt-0">Services</h2>
                     <hr className="divider" />
                     <div className="row gx-4 gx-lg-5">
                         <div className="col-lg-4 col-md-4 text-center">
@@ -77,7 +77,7 @@ const IndexPage = () => {
             <section className="page-section bg-brand-secondary" id="portfolio">
                 <div className="container">
                     <div className="text-center">
-                        <h2 className="section-heading text-brand-light">Portfolio</h2>
+                        <h2 className="section-heading text-white">Portfolio</h2>
                         <hr className="divider" />
                     </div>
                     <div className="row mt-5">
@@ -126,6 +126,28 @@ const IndexPage = () => {
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {/* Contact Section */}
+            <section className="page-section bg-brand-light" id="contact">
+                <div className="container px-4 px-lg-5">
+                    <div className="row gx-4 gx-lg-5 justify-content-center">
+                        <div className="col-lg-8 col-xl-6 text-center">
+                            <h2 className="mt-0">Let's Talk!</h2>
+                            <hr className="divider" />
+                            <p className="brand-text-grey mb-5">
+                                Ready to start your next project or have a few questions first? Contact us using the form below and I will get back to you as soon as possible!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row gx-4 gx-lg-5 justify-content-center">
+                        <div className="col-lg-4 text-center mb-5 mb-lg-0">
+                            <a className="btn btn-brand-primary btn-lg px-4" href="https://form.jotform.com/212678953278067" target="_blank" rel="noopener noreferrer">
+                                Contact Us
+                            </a>
                         </div>
                     </div>
                 </div>
