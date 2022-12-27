7f1e22350ea6d359dd70bdac896e7b07f75c6848
sheleyz <59847602+sheleyz@users.noreply.github.com>
2022-12-27 10:39:21 -0500

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,30 @@
-import * as React from "react"
-import { Link } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import * as React from "react";
+import Layout from "../components/Layout";
 
 const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    return (
+        <Layout>
+            <div className="text-white hero py-5">
+                <div className="container px-5">
+                    <div className="row gx-5 align-items-center justify-content-center">
+                        <div className="col-lg-7 col-xl-6">
+                            <div className="my-5 text-center text-lg-start">
+                                <h1 className="display-5 fw-bolder text-white mb-2">Page not found</h1>
+                                <p className="lead fw-normal text-grey mb-4">Sorry 😔, we couldn’t find what you were looking for.</p>
+                                <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-lg-start">
+                                    <a className="btn btn-outline-brand-primary btn-lg px-4" href="/">
+                                        Go home
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head = () => <title>Not found</title>
+export const Head = () => <title>Not found</title>;
