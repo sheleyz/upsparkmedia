730e98d245f3d385c12d2f446ab89f3de8f6bdb3
sheleyz <59847602+sheleyz@users.noreply.github.com>
2023-02-04 20:10:46 -0500

    Updated Footer social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,19 @@
 import * as React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
-import { faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
     return (
         <footer className="bg-brand-secondary py-5 text-white">
             <div className="container px-4 px-lg-5 fw-semibold">
-                <div className="text-center">UpSpark Media</div>
+                <div className="text-center fs-5">UpSpark Media</div>
                 <div className="text-center">
                     <a className="btn btn-social mx-2 my-3" href="https://twitter.com/upsparkmedia" target="_blank" rel="noopener noreferrer" title="UpSpark Media Twitter">
-                        <FontAwesomeIcon icon={faTwitterSquare} size="3x" fixedWidth />
+                        <FontAwesomeIcon icon={faTwitter} size="3x" fixedWidth />
                     </a>{" "}
                     <a className="btn btn-social mx-2 my-3" href="https://instagram.com/upspark.media" target="_blank" rel="noopener noreferrer" title="UpSpark Media Instagram">
-                        <FontAwesomeIcon icon={faInstagramSquare} size="3x" fixedWidth />
+                        <FontAwesomeIcon icon={faInstagram} size="3x" fixedWidth />
                     </a>
                 </div>
             </div>
