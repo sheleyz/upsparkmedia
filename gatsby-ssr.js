b72c8ea48b47205610f77197a2b1f1eec08ddfda
sheleyz <59847602+sheleyz@users.noreply.github.com>
2023-02-09 21:12:41 -0500

    Moved custom font to static folder, preload font

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,6 @@
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+import * as React from "react";
+
+export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
     setHtmlAttributes({ lang: "en-US" });
+    setHeadComponents([<link rel="preload" href="/fonts/Asap/Asap-VariableFont_wght.ttf" as="font" type="font/ttf" crossOrigin="anonymous" key="asapFont" />]);
 };
