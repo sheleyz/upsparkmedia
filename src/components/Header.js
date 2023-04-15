5fb3722a9c3f77f0fe35d7ca65298e01edcc4173
sheleyz <59847602+sheleyz@users.noreply.github.com>
2023-04-14 20:24:16 -0400

    Updated Navbar Toggle icon, footer credit, icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,12 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { StaticImage } from "gatsby-plugin-image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
     return (
-        <Navbar bg="brand-secondary" expand="md" className="py-3" variant="dark" fixed="top">
+        <Navbar bg="brand-secondary" collapseOnSelect expand="md" className="py-3" variant="dark" fixed="top">
             <Container className="px-4 px-md-5 mw-100">
                 <Navbar.Brand href="/">
                     <StaticImage
@@ -22,7 +24,7 @@ const Header = () => {
                         style={{ transition: "none" }}
                     />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbar-nav" />
+                <Navbar.Toggle aria-controls="navbar-nav" className="p-0"><FontAwesomeIcon icon={faBars} size="xl" fixedWidth /></Navbar.Toggle>
                 <Navbar.Collapse id="navbar-nav" className="my-2 my-md-0">
                     <Nav className="ms-auto">
                         <Nav.Link href="/#services">Services</Nav.Link>
