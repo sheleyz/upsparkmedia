0a7e0b9b6e29503156412ee1cbdf8e6fdc766cbb
sheleyz <59847602+sheleyz@users.noreply.github.com>
2025-01-20 10:21:31 -0500

    Added portfolio scss module file

--- src/components/portfolio/PortfolioItem.js
+++ src/components/portfolio/PortfolioItem.js
@@ -1,12 +1,16 @@
+"use client";
+
+import styles from "./portfolio.module.scss";
+
 export default function PortfolioItem({ image, heading, subheading, links }) {
     return (
         <div className="col-sm-12 col-md-6 col-lg-4 mb-4">
-            <div className="portfolio-item">
-                <div className="imageWrapper">{image}</div>
-                <div className="portfolio-caption">
-                    <div className="portfolio-caption-heading">{heading}</div>
-                    <div className="portfolio-caption-subheading text-grey-dark">{subheading}</div>
-                    <div className="portfolio-caption-links text-grey-dark">
+            <div className={`${styles["portfolio-item"]}`}>
+                <div className={`${styles["imageWrapper"]}`}>{image}</div>
+                <div className={`${styles["portfolio-caption"]}`}>
+                    <div className={`${styles["portfolio-caption-heading"]}`}>{heading}</div>
+                    <div className={`${styles["portfolio-caption-subheading"]} ${styles["text-grey-dark"]}`}>{subheading}</div>
+                    <div className={`${styles["portfolio-caption-links"]} ${styles["text-grey-dark"]}`}>
                         {links.map((item, index) => (
                             <div key={index} className="my-1 mx-2">
                                 <a href={item.link} target="_blank" rel="noopener noreferrer">
