063c1e3061df1d441d125fa4e395777fd1bd6791
sheleyz <59847602+sheleyz@users.noreply.github.com>
2025-08-27 21:33:07 -0400

    Removed middleware, removed blocking bots from robots.js, removed blocked page

--- src/app/blocked/page.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function BlockedPage() {
-    return (
-        <div className="text-white d-flex justify-content-center">
-            <h1>Blocked</h1>
-        </div>
-    );
-}
--- src/app/robots.js
+++ src/app/robots.js
@@ -1,16 +1,11 @@
 export default function robots() {
-    return {
-        rules: [
-            {
-                userAgent: ["Google-Extended", "Applebot-Extended", "ChatGPT-User", "GPTBot", "CCBot", "PerplexityBot", "anthropic-ai", "Claude-Web", "ClaudeBot", "Bytespider"],
-                disallow: "/"
-            },
-            {
-                userAgent: "*",
-                allow: "/",
-                disallow: "/blocked/"
-            }
-        ],
-        sitemap: "https://upsparkmedia.com/sitemap.xml"
-    };
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/"
+      }
+    ],
+    sitemap: "https://upsparkmedia.com/sitemap.xml"
+  };
 }
